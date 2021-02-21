2cfcb5e7a403591abc65c46c62b7caa04d237b70
wlademirdumaresq <wlademir.dumaresq@gmail.com>
2021-02-21 20:17:03 -0300

    Iniciando repositorios

---
+++ src/ufrn/imd/br/application/Main.java
@@ -0,0 +1,14 @@
+package ufrn.imd.br.application;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+    }
+}
