d2a48be484242a7978980f7fdd1c64e507d1abe9
wlademirdumaresq <wlademir.dumaresq@gmail.com>
2021-02-23 01:12:47 -0300

    ajuste LabirintoParaMatriz

--- src/ufrn/imd/br/util/LabirintoParaMatriz.java
+++ src/ufrn/imd/br/util/LabirintoParaMatriz.java
@@ -4,8 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class  LabirintoParaMatriz {
-    public static String[][] Matriz (){
+public class LabirintoParaMatriz {
+    public static String[][] Matriz() {
         String[][] labirinto_em_matriz;
         try {
             FileReader arq = new FileReader("src/ufrn/imd/br/application/labirintosTXT/labirinto01");
@@ -14,29 +14,32 @@ public class  LabirintoParaMatriz {
             int leitorLinhas = 0;
             int leitorColunas = 0;
 
-            while (leitor.hasNext()){
+            while (leitor.hasNext()) {
                 String line = leitor.nextLine();
-                leitorColunas=line.length();
-                leitorLinhas+=1;
+                leitorColunas = line.length();
+                leitorLinhas += 1;
             }
-            arq.close();
+
             FileReader arq2 = new FileReader("src/ufrn/imd/br/application/labirintosTXT/labirinto01");
             leitor = new Scanner(arq2);
-            labirinto_em_matriz = new String [leitorLinhas][leitorColunas];
+            labirinto_em_matriz = new String[leitorLinhas][leitorColunas];
             int i = 0;
-            while (leitor.hasNext()){
+            while (leitor.hasNext()) {
                 String line = leitor.nextLine();
                 for (int j = 0; j < leitorColunas; j++) {
-                    labirinto_em_matriz[i][j]= Character.toString(line.charAt(j));
+                    labirinto_em_matriz[i][j] = Character.toString(line.charAt(j));
 
                 }
-                i+=1;
+                i += 1;
             }
+            leitor.close();
+            arq2.close();
+            arq.close();
             return labirinto_em_matriz;
         } catch (IOException e) {
             System.out.println("parametro invalido" + e.getMessage());
         }
         return null;
-        }
+    }
 
 }
