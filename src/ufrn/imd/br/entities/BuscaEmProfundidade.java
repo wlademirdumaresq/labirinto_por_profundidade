185aefb4bb2d9620e2a46965b495880d234fca66
wlademirdumaresq <wlademir.dumaresq@gmail.com>
2021-02-23 21:06:07 -0300

    Correção de bug, criação de jar, implementação de args

--- src/ufrn/imd/br/entities/BuscaEmProfundidade.java
+++ src/ufrn/imd/br/entities/BuscaEmProfundidade.java
@@ -1,61 +1,70 @@
 package ufrn.imd.br.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuscaEmProfundidade {
 
     private final Labirinto labirinto;
     private final boolean[][] caminhosPercorridos;
+    private List<Localizacao> rotaFinal;
 
     public BuscaEmProfundidade(Labirinto labirinto) {
         this.labirinto = labirinto;
         this.caminhosPercorridos = new boolean[labirinto.getLinhas()][labirinto.getColunas()];
+        this.rotaFinal = new ArrayList<Localizacao>();
     }
 
     public boolean[][] getCaminhosPercorridos() {
         return caminhosPercorridos;
     }
 
-    public boolean validadorLocalizacao(int linha, int coluna){
+    public boolean validadorLocalizacao(int linha, int coluna) {
         return linha < 0 || linha >= this.labirinto.getLinhas() || coluna < 0 || coluna >= this.labirinto.getColunas();
     }
 
-    public void alterarValidadorRota(int linha, int coluna, boolean caminhou){
+    public void alterarValidadorRota(int linha, int coluna, boolean caminhou) {
         this.caminhosPercorridos[linha][coluna] = caminhou;
     }
-    public boolean checandoCaminho(int linha, int coluna){
+
+    public boolean checandoCaminho(int linha, int coluna) {
         return this.caminhosPercorridos[linha][coluna];
     }
 
-    public boolean percorrer(){
-       return  percorrer(this.labirinto, this.labirinto.getEntrada().getLinha(), this.labirinto.getEntrada().getColuna());
-    }
-    public boolean percorrer(Labirinto l, int linha, int coluna){
+    public boolean percorrer() {
 
-        if(validadorLocalizacao(linha,coluna) || this.labirinto.getMatriz()[linha][coluna].equals("#") || checandoCaminho(linha,coluna)){
+        return percorrer(this.labirinto, this.labirinto.getEntrada().getLinha(), this.labirinto.getEntrada().getColuna(), this.rotaFinal);
+    }
+
+    public boolean percorrer(Labirinto l, int linha, int coluna, List<Localizacao> lista) {
+
+        if (validadorLocalizacao(linha, coluna) || this.labirinto.getMatriz()[linha][coluna].equals("#") || checandoCaminho(linha, coluna)) {
             return false;
         }
-        this.alterarValidadorRota(linha,coluna,true);
+        this.alterarValidadorRota(linha, coluna, true);
+        lista.add(new Localizacao(linha, coluna));
 
-        if(l.getSaida().getLinha() == linha && l.getSaida().getColuna() == coluna){
+        if (l.getSaida().getLinha() == linha && l.getSaida().getColuna() == coluna) {
             return true;
         }
-        if (percorrer(l, linha, coluna + 1)) {
+        if (percorrer(l, linha, coluna + 1, lista)) {
             return true;
         }
-        if (percorrer(l, linha + 1, coluna)) {
+        if (percorrer(l, linha + 1, coluna, lista)) {
             return true;
         }
-        if (percorrer(l, linha, coluna - 1)) {
+        if (percorrer(l, linha, coluna - 1, lista)) {
             return true;
         }
-        if (percorrer(l, linha - 1, coluna)) {
+        if (percorrer(l, linha - 1, coluna, lista)) {
             return true;
         }
 
-//        this.alterarValidadorRota(linha,coluna,false);
+        lista.remove(lista.size() - 1);
         return false;
     }
 
-    public void printSolucao(){
+    public void printSolucao() {
         for (int i = 0; i < this.getCaminhosPercorridos().length; i++) {
             for (int j = 0; j < this.getCaminhosPercorridos()[0].length; j++) {
                 if (labirinto.getMatriz()[i][j].equals("E") || this.labirinto.getMatriz()[i][j].equals("S")) {
@@ -71,5 +80,22 @@ public class BuscaEmProfundidade {
             }
             System.out.println(" ");
         }
+    }
+
+    public void printRotaFinal() {
+        List<Localizacao> tmp = this.rotaFinal;
+        String tmpMatriz [][] = this.labirinto.getMatriz();
+        for (Localizacao x : tmp) {
+            if (this.labirinto.getEntrada().getColuna() == x.getColuna() && this.labirinto.getEntrada().getLinha() == x.getLinha()) {
+                continue;
+            }
+            if (this.labirinto.getSaida().getColuna() == x.getColuna() && this.labirinto.getSaida().getLinha() == x.getLinha()) {
+                continue;
+            }
+            tmpMatriz[x.getLinha()][x.getColuna()]="*";
+
+        }
+        Labirinto tmpLab = new Labirinto(tmpMatriz);
+        tmpLab.print();
     }
 }
--- src/ufrn/imd/br/entities/Localizacao.java
+++ src/ufrn/imd/br/entities/Localizacao.java
@@ -3,12 +3,19 @@ package ufrn.imd.br.entities;
 public class Localizacao {
     private final Integer linha;
     private final Integer coluna;
+    private Localizacao localizacaoAnterior;
 
     public Localizacao(Integer linha, Integer coluna) {
         this.linha = linha;
         this.coluna = coluna;
+        this.localizacaoAnterior = null;
     }
 
+    public Localizacao(Integer linha, Integer coluna, Localizacao localizacaoAnterior) {
+        this.linha = linha;
+        this.coluna = coluna;
+        this.localizacaoAnterior = localizacaoAnterior;
+    }
 
     public Integer getLinha() {
         return linha;
@@ -18,4 +25,7 @@ public class Localizacao {
         return coluna;
     }
 
+    public Localizacao getLocalizacaoAnterior() {
+        return localizacaoAnterior;
+    }
 }
